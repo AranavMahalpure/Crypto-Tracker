8a64d2859a6c49c80ecca2c0d6baea69638c0389
Arnav Mahalpure <100768252+AranavMahalpure@users.noreply.github.com>
2025-01-11 01:12:48 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -5,6 +5,8 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI, {
+            connectTimeoutMS: 30000,
+            socketTimeoutMS: 30000,
             useNewUrlParser: true,  
             useUnifiedTopology: true, 
         });
